254b544ddfc275067b6c38968806f9560e2afb73
syunyasuzuki <nkcgta.syunya.suzuki@gmail.com>
2020-07-08 15:01:54 +0900

    menuの改良

---
+++ Deep Snow/Assets/Fade/Scripts/UIFade_ctr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFade_ctr : MonoBehaviour
+{
+    [SerializeField] Fade fade = null;
+
+    //スタートメソッドより先に起動する
+    void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fade.FadeOut(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
