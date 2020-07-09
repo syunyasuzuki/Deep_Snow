469d6acf9fc8151bd78efdf3a58f89ef58f4d0c3
syunyasuzuki <nkcgta.syunya.suzuki@gmail.com>
2020-07-09 15:06:00 +0900

    UIfade作成

--- Deep Snow/Assets/Fade/Scripts/UIFade_ctr.cs
+++ Deep Snow/Assets/Fade/Scripts/UIFade_ctr.cs
@@ -6,21 +6,18 @@ public class UIFade_ctr : MonoBehaviour
 {
     [SerializeField] Fade fade = null;
 
-    //スタートメソッドより先に起動する
-    void Awake()
-    {
-
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        fade.FadeOut(1);
+        fade.FadeIn(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            fade.FadeOut(1.5f);
+        }
     }
 }
