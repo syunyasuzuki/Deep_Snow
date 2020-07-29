e0d79dc7d9b13fc792af044e1a1d24c5d19efae8
syunyasuzuki <nkcgta.syunya.suzuki@gmail.com>
2020-07-29 23:24:23 +0900

    タイトルシーン制作中

---
+++ Deep Snow/Assets/Script/TitleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] Fade fade = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            fade.FadeOut(1.0f);
+        }
+    }
+}
