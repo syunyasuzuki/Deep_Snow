fbfc0b56f368c5e9beff1aea1f00387bbdd56c4e
syunyasuzuki <nkcgta.syunya.suzuki@gmail.com>
2020-09-10 10:41:45 +0900

    タイトルシーン編集

---
+++ Deep Snow/Assets/Script/Cloud_ctr.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud_ctr : MonoBehaviour
+{
+    float cloudmove_x = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector2(Random.Range(-5.0f, 5.0f), Random.Range(-3.0f, 3.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
