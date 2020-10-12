112d27ab0343fd8c0d9dd4ea93de796db70f481b
hasegawaki <hk.lucy.0424@gmail.com>
2020-10-13 00:06:14 +0900

    新しく書き換えたスクリプトを追加

---
+++ Deep Snow/Assets/Hasegawa/Map_con.cs
@@ -0,0 +1,255 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map_con : MonoBehaviour
+{
+    /////<summary></summary>
+    
+    //テスト用(後で消す)
+    [SerializeField] int test_loadworld = 0;
+    [SerializeField] int test_loadstage = 0;
+
+    //ワールドの数
+    /// <summary>ワールドの数</summary>
+    const int World_num = 5;
+    /// <summary>一つのワールドのステージ数</summary>
+    const int Stage_num = 10;
+    //----------ファイル関係--------------------
+    /// <summary>ファイルのパス（アクセスはResourcesフォルダーから）</summary>
+    string[] FilePath = null;
+    /// <summary>ファイルのパスを設定</summary>
+    void Set_path(){
+        FilePath = new string[World_num] { "World1", "World2", "World3", "World4", "World5" };
+    }
+    //----------マップ関係--------------------
+    //マップサイズ
+    /// <summary>マップサイズ　X</summary>
+    const int Mapsize_x = 32;
+    /// <summary>マップサイズ　Y</summary>
+    const int Mapsize_y = 18;
+    /// <summary>マップの大きさを返す</summary>
+    public void Read_mapsize(ref int x,ref int y){
+        x = Mapsize_x;
+        y = Mapsize_y;
+    }
+    /// <summary>全てのマップデータ</summary>
+    int[,,,] map = new int[World_num, Stage_num, Mapsize_y, Mapsize_x];
+
+    /// <summary>一括で全ステージを読み込む（データがリソースファイルにある前提）</summary>
+    void Read_all_maps(){
+        for (int w = 0; w < World_num; ++w){
+            string text_data = (Resources.Load(FilePath[w], typeof(TextAsset)) as TextAsset).text;
+            string[] text_line = text_data.Split('\n');
+            for (int s = 0; s < Stage_num; ++s){
+                for(int y = 0; y < Mapsize_y; ++y){
+                    string[] strsplr = text_line[y].Split(',');
+                    for(int x = 0; x < Mapsize_x; ++x){
+                        map[w, s, y, x] = int.Parse(strsplr[x]);
+                    }
+                }
+            }
+        }
+    }
+    //素材
+    ///<summary>ノーマルブロック</summary>
+    [SerializeField] Sprite[] Normal_block = new Sprite[3];
+    ///<summary>雪の積もってるブロック</summary>
+    [SerializeField] Sprite[] Falled_block = new Sprite[4];
+    /// <summary>各マップチップの素材</summary>
+    [SerializeField] Sprite[] Blocks = new Sprite[11];
+    /// <summary>壊れる床の素材</summary>
+    [SerializeField] Sprite[] Broken_ice = new Sprite[8];
+    /// <summary>マップチップを入れる親オブジェクト</summary>
+    GameObject Map_mother = null;
+    ///<summary>一回でもマップを生成したか</summary>
+    bool create_map = false;
+    ///<summary>現在使っているワールド番号</summary>
+    int now_world = 0;
+    ///<summary>現在使っているステージ番号</summary>
+    int now_stage = 0;
+    /// <summary>現在使っているマップ</summary>
+    int[,] now_map = new int[Mapsize_y, Mapsize_x];
+    ///<summary>現在使っているマップの</summary>
+    public int Read_mapchip(int x,int y){
+        return now_map[y, x];
+    }
+    ///<summary>現在使っている壊れる床の数</summary>
+    int br_num = 0;
+    ///<summary>現在使っている壊れる床の位置</summary>
+    Vector3[] br_pos;
+    ///<summary>各壊れる床のスプライト</summary>
+    SpriteRenderer[] br_blocks;
+    ///<summary>各壊れる床の状態（0 = 生成された状態 , 1 = 壊れてる , 2 = 壊れた）</summary>
+    int[] br_mode;
+    ///<summary>各壊れる床のスプライト番号</summary>
+    int[] spr_count;
+    ///<summary>各壊れる床のアニメーションカウント</summary>
+    int[] br_count;
+    ///<summary>壊れる床の数を返す</summary>
+    public int Read_br(){
+        return br_num;
+    }
+    ///<summary>指定された壊れる床の位置を返す</summary>
+    public Vector3 Read_br_pos(int x){
+        return br_pos[x];
+    }
+    ///<summary>
+    ///壊れる床のアニメーションを有効にする
+    ///現在使っているマップ情報も書き換える
+    /// </summary>
+    public void Set_br_animation(int x){
+        br_mode[x] = 1;
+        map[now_world, now_stage, (int)-br_pos[x].y, (int)br_pos[x].x] = 0;
+    }
+    ///<summary>壊れる床の処理</summary>
+    void Br_blocks_task(){
+        for(int lu = 0; lu < br_num; ++lu){
+            if (br_mode[lu] == 1){
+                if (++br_count[lu] >= 4){
+                    br_count[lu] = 0;
+                    if (++spr_count[lu] >= 8){
+                        br_blocks[lu] = null;
+                        br_mode[lu] = 2;
+                    }
+                    else{
+                        br_blocks[lu].sprite = Broken_ice[spr_count[lu]];
+                    }
+                }
+            }
+        }
+    }
+    ///<summary>現在使っているマップの棘の数</summary>
+    int spike_num = 0;
+    ///<summary>現在使っている棘の位置</summary>
+    Vector3[] spike_pos = new Vector3[500];
+    ///<summary>棘の数を返す</summary>
+    public int Read_spike(){
+        return spike_num;
+    }
+    ///<summary>棘の位置を返す</summary>
+    public Vector3 Read_spike_pos(int x){
+        return spike_pos[x];
+    }
+    ///<summary>積もる雪が生成できるブロックの数</summary>
+    int fed_snw = 0;
+    ///<summary>積もる雪の位置</summary>
+    Vector3[] fed_snw_pos = new Vector3[500];
+    ///<summary>積もる位置の雪の番号</summary>
+    int[] fed_snw_num = new int[500];
+    ///<summary>積もる雪の数を返す</summary>
+    public int Read_fed_snw(){
+        return fed_snw;
+    }
+    ///<summary>積もる雪の位置を返す</summary>
+    public Vector3 Read_fed_snw_pos(int x){
+        return fed_snw_pos[x];
+    }
+    ///<summary>積もる場所の番号を返す</summary>
+    public int Read_fed_snw_num(int x){
+        return fed_snw_num[x];
+    }
+
+    ///<summary>積もる雪の数、雪の位置を割り出す</summary>
+    void Sarch_fed_snw(int w,int s){
+        fed_snw = 0;
+        for(int na = 0; na < Mapsize_y; ++na){
+            for(int lu = 0; lu < Mapsize_x; ++lu){
+                if (map[w, s, lu, na] != 0 && map[w,s,lu,na] != 10){
+                    if (lu - 1 >= 0){
+                        if (map[w, s, lu - 1, na] == 0 || (map[w, s, lu - 1, na] >= 3 && map[w, s, lu - 1, na] <= 6) || map[w, s, lu - 1, na] == 10){
+                            fed_snw_pos[fed_snw] = new Vector3(na, -lu + 1 - 0.5f, 0.0f);
+                            fed_snw_num[fed_snw] = map[w, s, lu - 1, na];
+                            ++fed_snw;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    /// <summary>指定されたチップを生成</summary>
+    /// <param name="n">生成するオブジェクトの番号</param>
+    public void Create_chip(int x, int y, int n){
+        switch (n){
+            case 1:
+                int rnd = Random.Range(0, 2);
+                GameObject n_bl = new GameObject("nb_bl " + x + " - " + y);
+                n_bl.AddComponent<SpriteRenderer>().sprite = Normal_block[rnd];
+                n_bl.transform.position = new Vector3(x, -y, 0.0f);
+                n_bl.transform.parent = Map_mother.transform;
+                break;
+            case 2:
+                int rnd2 = Random.Range(0, 3);
+                GameObject f_bl = new GameObject("f_bl " + x + " - " + y);
+                f_bl.AddComponent<SpriteRenderer>().sprite = Falled_block[rnd2];
+                f_bl.transform.position = new Vector3(x, -y, 0.0f);
+                f_bl.transform.parent = Map_mother.transform;
+                break;
+            case 10:
+                GameObject s_bl = new GameObject("s_bl " + x + " - " + y);
+                s_bl.AddComponent<SpriteRenderer>().sprite = Blocks[10];
+                s_bl.transform.position = new Vector3(x, -y, 0.0f);
+                s_bl.transform.parent = Map_mother.transform;
+                spike_pos[spike_num] = new Vector3(x, -y, 0.0f);
+                ++spike_num;
+                break;
+            case 11:
+                GameObject br = new GameObject("br_bl_" + br_num);
+                br.AddComponent<SpriteRenderer>().sprite = Broken_ice[0];
+                br.transform.position = new Vector3(x, -y, 0.0f);
+                br.transform.parent = Map_mother.transform;
+                br_pos[br_num] = new Vector3(x, -y, 0.0f);
+                ++br_num;
+                break;
+            default:
+                GameObject bl = new GameObject("bl " + x + " - " + y);
+                bl.AddComponent<SpriteRenderer>().sprite = Blocks[n];
+                bl.transform.position = new Vector3(x, -y, 0.0f);
+                bl.transform.parent = Map_mother.transform;
+                break;
+        }
+    }
+    /// <summary>指定されたマップを生成</summary>
+    /// <param name="w">生成するワールド</param>
+    /// <param name="s">生成するステージ</param>
+    public void Create_map(int w, int s){
+        Snow_con snc = GetComponent<Snow_con>();
+        if (create_map){
+            Destroy(Map_mother.gameObject);
+            snc.Destroy_allsnow();
+        }
+        Map_mother = new GameObject("map_mother");
+        br_num = 0;
+        spike_num = 0;
+        for (int lu = 0; lu < Mapsize_y; ++lu){
+            for (int na = 0; na < Mapsize_x; ++na){
+                Create_chip(na, lu, now_map[lu,na] = map[w, s, lu, na]);
+            }
+        }
+        Sarch_fed_snw(w, s);
+        snc.Set_all_snows();
+        create_map = true;
+    }
+    /// <summary>現在使われているマップを消す</summary>
+    public void Delete_map(){
+        GameObject map_mother = GameObject.Find("map_mother");
+        Destroy(map_mother.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        Set_path();
+        Read_all_maps();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Debug_mode();
+    }
+
+    /// <summary>デバッグ用（あとで消す！！！）</summary>
+    void Debug_mode(){
+        if (Input.GetKeyDown(KeyCode.D)) Create_map(test_loadworld, test_loadstage);
+    }
+}
---
+++ Deep Snow/Assets/Hasegawa/Snow_con.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class Snow_con : MonoBehaviour
+{
+    /////<summary></summary>
+
+    /// <summary>マップデータを受け取る参照先</summary>
+    Map_con Map_ob = null;
+
+    /// <summary>降る雪の数</summary>
+    const int Max_fallsnows = 100;
+    /// <summary>積もる雪の分割数</summary>
+    const int Sprit_falledsnow = 25;
+    /// <summary>雲の大きさ</summary>
+    const float CloudSize_x = 1.0f;
+
+    /// <summary>生成する雪</summary>
+    [SerializeField] GameObject ob_snow = null;
+    /// <summary>生成する雲</summary>
+    [SerializeField] Sprite spr_cloud = null;
+
+    ///<summary>ループ処理を止める</summary>
+    bool snow_task = false;
+    /// <summary>生成した積もる雪</summary>
+    GameObject[] falled_snow = null;
+    /// <summary>生成した積もる雪を入れる親オブジェクト</summary>
+    GameObject Master_falled_snow = null;
+    /// <summary>生成した降る雪</summary>
+    GameObject[] fall_snow = new GameObject[Max_fallsnows];
+    ///<summary>各降る雪の落下速度</summary>
+    float[] fall_spd = new float[Max_fallsnows];
+    ///<summary>各降る雪の状態（0 = 停止 , 1 = 落下 , 2 = 停止待機）</summary>
+    int[] fall_mode = new int[Max_fallsnows];
+    /// <summary>生成した降る雪を入れる親オブジェクト</summary>
+    GameObject Master_fall_snow = null;
+    /// <summary>生成した雲</summary>
+    GameObject Cloud = null;
+
+    //マップの大きさ
+    /// <summary>マップの大きさ　X</summary>
+    private int map_size_x = 32;
+    /// <summary>マップの大きさ　Y</summary>
+    private int map_size_y = 18;
+    ///<summary>積もる雪を置く位置</summary>
+    int[,] snw_pos = new int[4, Sprit_falledsnow]{
+        {25,25,24,23,22,20,19,19,17,17,16,15,14,13,12,11,10,9,8,6,6,5,4,2,1 },
+        {1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,17,19,19,20,22,23,24,25,25 },
+        {14,15,15,16,16,17,17,17,18,18,19,19,20,21,21,21,22,22,23,23,24,24,24,25,25 },
+        {1,1,2,3,3,3,4,4,5,6,7,7,7,8,8,9,9,10,10,11,11,11,12,13,14 }
+    };
+    ///<summary>積もる雪の数</summary>
+    int fed_snw;
+    ///<summary>積もる雪の位置</summary>
+    Vector3[] fed_snw_pos;
+    ///<summary>積もる雪を置く地点の番号</summary>
+    int[] fed_snw_num;
+    /// <summary>マップから必要なデータをもらう</summary>
+    void Set_Map_con(){
+        Map_ob = GetComponent<Map_con>();
+    }
+    /// <summary>積もる雪を準備</summary>
+    void Set_falledSnows(){
+        fed_snw = Map_ob.Read_fed_snw();
+        fed_snw_pos = new Vector3[fed_snw];
+        fed_snw_num = new int[fed_snw];
+        for(int lu = 0; lu < fed_snw; ++lu){
+            fed_snw_pos[lu] = Map_ob.Read_fed_snw_pos(lu);
+            fed_snw_num[lu] = Map_ob.Read_fed_snw_num(lu);
+        }
+        Master_falled_snow = new GameObject("falled_snow");
+        falled_snow = new GameObject[fed_snw * Sprit_falledsnow];
+        for(int lu = 0; lu < fed_snw; ++lu){
+            for(int na = 0; na < Sprit_falledsnow; ++na){
+                falled_snow[lu * Sprit_falledsnow + na] = Instantiate(ob_snow);
+                falled_snow[lu * Sprit_falledsnow + na].transform.parent = Master_falled_snow.transform;
+                float sub = 1.0f / Sprit_falledsnow;
+                falled_snow[lu * Sprit_falledsnow + na].transform.localScale = new Vector3(sub, sub, 0.0f);
+                switch (fed_snw_num[lu]){
+                    case 0:
+                        falled_snow[lu * Sprit_falledsnow + na].transform.position = new Vector3(fed_snw_pos[lu].x - 0.5f + sub * na + sub / 2, fed_snw_pos[lu].y, 0.0f);
+                        break;
+                    case 10:
+                        falled_snow[lu * Sprit_falledsnow + na].transform.position = new Vector3(fed_snw_pos[lu].x - 0.5f + sub * na + sub / 2, fed_snw_pos[lu].y, 0.0f);
+                        break;
+                    default:
+                        falled_snow[lu * Sprit_falledsnow + na].transform.position = new Vector3(fed_snw_pos[lu].x - 0.5f + sub * na + sub / 2, fed_snw_pos[lu].y + sub * snw_pos[fed_snw_num[lu] - 3, na], 0.0f);
+                        break;
+                }
+            }
+        }
+    }
+    /// <summary>雲と降る雪を準備</summary>
+    void Set_fallsnows(){
+        Cloud = new GameObject("cloud");
+        Cloud.AddComponent<SpriteRenderer>().sprite = spr_cloud;
+        Cloud.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        Cloud.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        Master_fall_snow = new GameObject("fall_snows");
+        for(int lu = 0; lu < Max_fallsnows; ++lu){
+            fall_snow[lu] = Instantiate(ob_snow);
+            fall_snow[lu].transform.position = new Vector3(-1.0f, 1.0f, 0.0f);
+            fall_snow[lu].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            fall_snow[lu].transform.parent = Master_fall_snow.transform;
+            fall_spd[lu] = Random.Range(0.05f, 0.1f);
+            fall_mode[lu] = 0;
+        }
+    }
+
+    /// <summary>積もる雪と降る雪と雲を消す</summary>
+    public void Destroy_allsnow(){
+        Destroy(Master_fall_snow.gameObject);
+        Destroy(Master_falled_snow.gameObject);
+        Destroy(Cloud.gameObject);
+        snow_task = false;
+    }
+    /// <summary>降る雪の処理</summary>
+    Vector3 plus_snow = new Vector3(0.0f, 0.05f, 0.0f);
+    /// <summary>降る雪の処理</summary>
+    void Fallsnows_task(){
+        Vector3 c_pos = Cloud.transform.position;
+        for(int lu = 0; lu < Max_fallsnows; ++lu){
+            if (fall_mode[lu] != 0){
+                fall_snow[lu].transform.position -= new Vector3(0.0f, fall_spd[lu], 0.0f);
+                if (fall_snow[lu].transform.position.y < -map_size_y){
+                    if (fall_mode[lu] == 2){
+                        fall_snow[lu].transform.position = new Vector3(-1.0f, 1.0f, 0.0f);
+                        fall_mode[lu] = 0;
+                    }
+                    else{
+                        fall_snow[lu].transform.position = new Vector3(c_pos.x + Random.Range(-CloudSize_x, CloudSize_x), c_pos.y, 0.0f);
+                    }
+                }
+                
+            }
+        }
+    }
+    ///<summary>雪を降らす</summary>
+    void Start_fall_snows(){
+        Vector3 c_pos = Cloud.transform.position;
+        for(int lu = 0; lu < Max_fallsnows; ++lu){
+            fall_snow[lu].transform.position = new Vector3(c_pos.x + Random.Range(-CloudSize_x, CloudSize_x), c_pos.y, 0.0f);
+            fall_mode[lu] = 1;
+        }
+    }
+    ///<summary>雪を止ます（今降っている分は降り続ける）</summary>
+    void  Stop_fall_snows(){
+        for(int lu = 0; lu < Max_fallsnows; ++lu){
+            fall_mode[lu] = 2;
+        }
+    }
+    ///<summary>雪を止める</summary>
+    void Stop_fall_snows_c(){
+        for(int lu = 0; lu < Max_fallsnows; ++lu){
+            fall_mode[lu] = 0;
+        }
+    }
+    ///<summary>雪をリセット</summary>
+    public void Reset_fall_snows(){
+        for(int lu = 0; lu < Max_fallsnows; ++lu){
+            fall_snow[lu].transform.position = new Vector3(-1.0f, 1.0f, 0.0f);
+            fall_mode[lu] = 0;
+        }
+    }
+    ///<summary>カメラ</summary>
+    Camera maincam;
+    ///<summary>カメラを取得</summary>
+    void Set_camera(){
+        maincam = Camera.main;
+    }
+    /// <summary>雲を動かす処理</summary>
+    void Move_clouds_task(){
+        Vector3 m_position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(maincam.transform.position.z));
+        Vector3 w_position = Camera.main.ScreenToWorldPoint(m_position);
+        int m_x = Mathf.FloorToInt(w_position.x), m_y = Mathf.FloorToInt(w_position.y + 0.5f);
+        Cloud.transform.position = new Vector3(m_x + 0.5f , m_y, 0.0f);
+        if (Input.GetMouseButtonDown(0)) { Start_fall_snows(); }
+        if (Input.GetMouseButtonUp(0)) { Stop_fall_snows(); }
+    }
+    ///<summary>マップ作成時に実行</summary>
+    public void Set_all_snows(){
+        Set_falledSnows();
+        Set_fallsnows();
+        snow_task = true;
+    }
+    // Start is called before the first frame update
+    void Start(){
+        Set_Map_con();
+        Set_camera();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (snow_task){
+            Fallsnows_task();
+            Move_clouds_task();
+        }
+        if (Input.GetKeyDown(KeyCode.H)) { Debug.Log(falled_snow.Length); }
+    }
+}
